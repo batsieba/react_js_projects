f7a9a958ce4457be9b26d7794636b52f00c5e14f
batsieba <batisebatekle@gmail.com>
2023-10-15 16:49:26 +0400

    front end

---
+++ millionaire_quiz_app/src/components/Trivia.jsx
@@ -0,0 +1,14 @@
+
+export default function Trivia() {
+  return (
+    <div className="trivia">
+        <div className="question">What is the best youtube channel?</div>
+        <div className="answers">
+            <div className="answer">Lama Dev</div>
+            <div className="answer">Great Stack</div>
+            <div className="answer">Beta Squad</div>
+            <div className="answer">Storniolo Brothers</div>
+        </div>
+    </div>
+  )
+}
